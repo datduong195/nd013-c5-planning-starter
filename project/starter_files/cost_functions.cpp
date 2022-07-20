0decda284ea572dee789114c4579b6d6a90cc143
Vy Nguyen <datduong195@gmail.com>
2022-07-20 18:06:52 +0200

    Added: implementation of close_to_main_goal_cost_spiral

--- project/starter_files/cost_functions.cpp
+++ project/starter_files/cost_functions.cpp
@@ -95,9 +95,9 @@ double close_to_main_goal_cost_spiral(const std::vector<PathPoint>& spiral,
   // 1].y and spiral[n - 1].z.
   // Use main_goal.location.x, main_goal.location.y and main_goal.location.z
   // Ex: main_goal.location.x - spiral[n - 1].x
-  auto delta_x = 0;  // <- Update
-  auto delta_y = 0;  // <- Update
-  auto delta_z = 0;  // <- Update
+  auto delta_x = main_goal.location.x - spiral[n-1].x;  // <- Update
+  auto delta_y = main_goal.location.y - spiral[n-1].y;  // <- Update
+  auto delta_z = main_goal.location.z - spiral[n-1].z;  // <- Update
 
   auto dist = std::sqrt((delta_x * delta_x) + (delta_y * delta_y) +
                         (delta_z * delta_z));
